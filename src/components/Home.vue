2bf94b930928de96dcebf8ae018876404cee7f93
smlw <smlwmy@gmail.com>
2019-04-21 11:05:51 +0500

    configure router & vuex
    Signed-off-by: smlw <smlwmy@gmail.com>

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.hello
+  h1 {{ msg }}
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Home page'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+a
+  color red
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
 new Vue({
   render: h => h(App),
+  store,
+  router
 }).$mount('#app')
