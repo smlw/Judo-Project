d4d690d057443a385fd8bbee1ec6548331ace112
smlw <smlwmy@gmail.com>
2019-04-23 16:39:23 +0500

    Блок gallery
    Signed-off-by: smlw <smlwmy@gmail.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,25 +19,40 @@
             p 
               img(src="../assets/images/events_1.png")
               span Основной целью деятельности школы является образовательная деятельность по дополнительным общеобразовательным программам в сфере физической культуры и спорта для детей до 18 лет: бокс, гандбол, дзюдо, карате, плавание, футбол. Основным видом деятельности Учреждения является дополнительное образование детей и взрослых: 1) реализация дополнительных предпрофессиональных программ 1) реализация дополнительных предпрофессиональных программ в области физической культуры и спорта; 2) реализация дополнительных общеразвивающих программ. Обучение в спортивной школе "Буревестник" ведется на бесплатной основе.
+        
+        // Знаю, что в этом блоке что-то не так и так нельзя делать, но пока придумать лучше не смог. Нужен совет
         .gallery
           .gallery_photo
             h2 Фото
             .gallery_photo_content
               .gallery_photo_content_main
-                .events_cards_one
-                  img(src="../assets/images/events_2.png")
-                  .events_cards_one_description
-                    .events_cards_one_description_gradient
-                      .events_cards_one_description_date
-                        h3 14 апреля 2018
-                      .events_cards_one_description_text
-                        p Дзюдо: турнир памяти Н.С. Мусатова
+                Card( width="460px", height="269px", margin="-269px"
+                      :img="galleryCard[0].img"
+                      :linkTo="galleryCard[0].linkTo"
+                    )
+                  h3(slot="date") {{ galleryCard[0].date }}
+                  p(slot="text") {{ galleryCard[0].text }}
               .gallery_photo_content_second
+                Card( width="181px", height="129px", margin="-129px"
+                  :img="galleryCard[1].img"
+                  :linkTo="galleryCard[1].linkTo"
+                )
+                  h3(slot="date") {{ galleryCard[1].date }}
 
+                Card( width="181px", height="129px", margin="-129px"
+                  :img="galleryCard[2].img"
+                  :linkTo="galleryCard[2].linkTo"
+                )
+                  h3(slot="date") {{ galleryCard[2].date }}
           .gallery_video
             h2 Видео
             .gallery_video_content
-              
+              Card( width="364px", height="269px", margin="-269px"
+                    :img="galleryCard[0].img"
+                    :linkTo="galleryCard[0].linkTo"
+              )
+                h3(slot="date") {{ galleryCard[0].date }}
+                p(slot="text") {{ galleryCard[0].text }}
 </template>
 
 <script>
@@ -53,7 +68,12 @@ export default {
         { linkTo: '/link/1', img: '~/../../assets/images/events_1.png', date: '12 апреля 2018', text: 'С 22 апреля по 18 мая 2019 года в Свердловской области пройдет эко-марафон переработка «Сдай макулатуру – спаси дерево!». Основная задача акции – привлечь внимание людей к ресурсосбережению, заставить задуматься над...'},
         { linkTo: '/link/2', img: '123', date: '13 апреля 2018', text: 'Дзюдо: мастер класс от Колесникова Сергея Викторовича'},
         { linkTo: '/link/3', img: '123', date: '14 апреля 2018', text: 'Основная задача акции – привлечь внимание людей к ресурсосбережению, заставить задуматься над...'}
-      ]
+      ],
+      galleryCard: [
+        { linkTo: '/link/1', img: '123', date: '12 апреля 2018', text: 'С 22 апреля по 18 мая 2019 года в Свердловской области пройдет эко-марафон переработка «Сдай макулатуру – спаси дерево!». Основная задача акции – привлечь внимание людей к ресурсосбережению, заставить задуматься над...'},
+        { linkTo: '/link/2', img: '123', date: '13 апреля 2018', text: 'Дзюдо: мастер класс от Колесникова Сергея Викторовича'},
+        { linkTo: '/link/3', img: '123', date: '14 апреля 2018', text: 'Основная задача акции – привлечь внимание людей к ресурсосбережению, заставить задуматься над...'}
+      ],
     }
   },
   components: { MainLayout, Card }
@@ -70,10 +90,25 @@ h2
   justify-content space-between
   align-items flex-start
   align-content stretch
+  h3
+    font-size 18px
+    line-height normal
+  p
+    font-size 20px
   &_photo
-    flex-grow 2
-  &_video
-    flex-grow 1
+    &_content
+      display flex
+      flex-direction row
+      flex-wrap nowrap
+      justify-content space-between
+      align-items flex-start
+      align-content stretch
+      &_main
+        a
+          margin 0 11px 0 0
+      &_second
+        a
+          margin 0 0 11px 0
 .about
   margin 0 0 95px 0
   &_content
--- src/components/elements/Card.vue
+++ src/components/elements/Card.vue
@@ -17,6 +17,21 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.gallery_video_content
+  
+.gallery_photo_content
+  &_main
+    .card_description_gradient
+      padding 70px 18px 12px
+    .card_description_text
+      height 65%
+  &_second
+    .card_description
+      &:hover
+        .card_description_gradient
+          padding 50px 5px 5px
+      &_gradient
+        padding 50px 5px 5px
 .card
   img 
     height 100%
