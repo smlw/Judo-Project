b39841ea306552ab22d074008514b1ec9da83261
smlw <smlwmy@gmail.com>
2019-04-23 15:44:16 +0500

    create card component
    Signed-off-by: smlw <smlwmy@gmail.com>

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,34 +5,13 @@
         .events
           h2 События
           .events_cards
-            a(href="/")
-              .events_cards_one
-                img(src="../assets/images/events_1.png")
-                .events_cards_one_description
-                  .events_cards_one_description_gradient
-                    .events_cards_one_description_date
-                      h3 13апреля 2018
-                    .events_cards_one_description_text
-                      p С 22 апреля по 18 мая 2019 года в Свердловской области пройдет эко-марафон переработка «Сдай макулатуру – спаси дерево!». Основная задача акции – привлечь внимание людей к ресурсосбережению, заставить задуматься над...
-            a(href="/")
-              .events_cards_one
-                img(src="../assets/images/events_2.png")
-                .events_cards_one_description
-                  .events_cards_one_description_gradient
-                    .events_cards_one_description_date
-                      h3 14 апреля 2018
-                    .events_cards_one_description_text
-                      p Дзюдо: турнир памяти Н.С. Мусатова
-            a(href="/")
-              .events_cards_one
-                img(src="../assets/images/events_3.png")
-                .events_cards_one_description
-                  .events_cards_one_description_gradient
-                    .events_cards_one_description_date
-                      h3 15 апреля 2018
-                    .events_cards_one_description_text
-                      p Дзюдо: мастер класс от Колесникова Сергея Викторовича
-
+            Card( v-for="(card, index) in eventCard" :key="index"
+                  width="365px", height="256px", margin="-256px"
+                  :img="card.img"
+                  :linkTo="card.linkTo"
+                )
+              h3(slot="date") {{ card.date }}
+              p(slot="text") {{ card.text }}
 
         .about
           h2 Об отделении дзюдо в ДЮСШ "Буревестник"
@@ -40,25 +19,61 @@
             p 
               img(src="../assets/images/events_1.png")
               span Основной целью деятельности школы является образовательная деятельность по дополнительным общеобразовательным программам в сфере физической культуры и спорта для детей до 18 лет: бокс, гандбол, дзюдо, карате, плавание, футбол. Основным видом деятельности Учреждения является дополнительное образование детей и взрослых: 1) реализация дополнительных предпрофессиональных программ 1) реализация дополнительных предпрофессиональных программ в области физической культуры и спорта; 2) реализация дополнительных общеразвивающих программ. Обучение в спортивной школе "Буревестник" ведется на бесплатной основе.
+        .gallery
+          .gallery_photo
+            h2 Фото
+            .gallery_photo_content
+              .gallery_photo_content_main
+                .events_cards_one
+                  img(src="../assets/images/events_2.png")
+                  .events_cards_one_description
+                    .events_cards_one_description_gradient
+                      .events_cards_one_description_date
+                        h3 14 апреля 2018
+                      .events_cards_one_description_text
+                        p Дзюдо: турнир памяти Н.С. Мусатова
+              .gallery_photo_content_second
+
+          .gallery_video
+            h2 Видео
+            .gallery_video_content
+              
 </template>
 
 <script>
 import MainLayout from '../components/layouts/MainLayout'
+import Card from '../components/elements/Card'
 export default {
   name: 'Home',
   data () {
     return {
       h1: 'Шрифт заголовков Behrens AntiqaC',
-      p: 'Шрифт основного текста'
+      p: 'Шрифт основного текста',
+      eventCard: [
+        { linkTo: '/link/1', img: '~/../../assets/images/events_1.png', date: '12 апреля 2018', text: 'С 22 апреля по 18 мая 2019 года в Свердловской области пройдет эко-марафон переработка «Сдай макулатуру – спаси дерево!». Основная задача акции – привлечь внимание людей к ресурсосбережению, заставить задуматься над...'},
+        { linkTo: '/link/2', img: '123', date: '13 апреля 2018', text: 'Дзюдо: мастер класс от Колесникова Сергея Викторовича'},
+        { linkTo: '/link/3', img: '123', date: '14 апреля 2018', text: 'Основная задача акции – привлечь внимание людей к ресурсосбережению, заставить задуматься над...'}
+      ]
     }
   },
-  components: { MainLayout }
+  components: { MainLayout, Card }
 }
 </script>
 
 <style lang="stylus" scoped>
 h2
   margin 0 0 25px 0
+.gallery
+  display flex
+  flex-direction row
+  flex-wrap nowrap
+  justify-content space-between
+  align-items flex-start
+  align-content stretch
+  &_photo
+    flex-grow 2
+  &_video
+    flex-grow 1
 .about
   margin 0 0 95px 0
   &_content
@@ -70,7 +85,8 @@ h2
       height 258px
       float left
       padding 0 24px 15px 0 
-.events
+.events,
+.gallery
   width 100%
   margin 0 0 95px 0
 .events_cards
@@ -80,42 +96,4 @@ h2
   justify-content space-between
   align-items flex-start
   align-content stretch
-  &_one
-    width 365px
-    height 256px
-    &_description
-      &:hover
-        .events_cards_one_description_gradient
-          transition all 0.25s ease
-          background rgba(0,0,0,0.7)
-          height 100%
-          padding 11px 18px
-          .events_cards_one_description_text
-            transition all 0.25s ease
-            height 100%
-      height 256px
-      margin -256px 0 0 0
-      display flex
-      flex-direction column
-      flex-wrap wrap
-      justify-content flex-end
-      align-content flex-start
-      align-items flex-start
-      line-height normal
-      font-family 'Noto Sans', sans-serif
-      color #fff
-      &_date
-        font-size 24px
-        font-weight bold
-      &_text
-        font-size 18px
-        height 50%
-        overflow hidden
-      &_gradient
-        height 60%
-        overflow hidden
-        background rgba(0,0,0,0.7)
-        background linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 96.87%)
-        box-sizing border-box
-        padding 50px 18px 12px
 </style>
---
+++ src/components/elements/Card.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+  a(:href="`${this.linkTo}`")
+    .card(:style="`width: ${this.width}; height: ${this.height}`")
+      img(:src="`${this.img}`")
+      .card_description(:style="`height: ${this.height}; margin-top: ${this.margin}`")
+        .card_description_gradient
+          .card_description_date
+            slot(name="date")
+          .card_description_text
+            slot(name="text")
+</template>
+
+<script>
+export default {
+  props: ['width', 'height', 'margin', 'img', 'linkTo']
+}
+</script>
+
+<style lang="stylus" scoped>
+.card
+  img 
+    height 100%
+  &_description
+    &:hover
+      .card_description_gradient
+        transition all 0.25s ease
+        background rgba(0,0,0,0.7)
+        height 100%
+        padding 11px 18px
+        .card_description_text
+          transition all 0.25s ease
+          height 100%
+    display flex
+    flex-direction column
+    flex-wrap wrap
+    justify-content flex-end
+    align-content flex-start
+    align-items flex-start
+    line-height normal
+    font-family 'Noto Sans', sans-serif
+    color #fff
+    &_date
+      font-size 24px
+      font-weight bold
+    &_text
+      font-size 18px
+      height 50%
+      overflow hidden
+    &_gradient
+      height 60%
+      width 100%
+      overflow hidden
+      background rgba(0,0,0,0.7)
+      background linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.7) 96.87%)
+      box-sizing border-box
+      padding 50px 18px 12px
+</style>
+
