bee202130ff64c884fa1ce0b9d4263a0872bbae8
smlw <smlwmy@gmail.com>
2019-04-24 19:10:16 +0500

    article page done
    Signed-off-by: smlw <smlwmy@gmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import VideoAlbum from '../components/pages/VideoAlbum.vue'
 import OneNew from '../components/pages/OneNew.vue'
 import Trainer from '../components/pages/Trainer.vue'
 import Human from '../components/pages/Human.vue'
+import Article from '../components/pages/Article.vue'
 
 const router = new Router({
   mode: 'history',
@@ -42,6 +43,11 @@ const router = new Router({
       path: '/human/:id',
       component: Human,
       meta: {title: 'Human'}
+    },
+    {
+      path: '/article/:id',
+      component: Article,
+      meta: {title: 'Статья'}
     }
   ]
 })
