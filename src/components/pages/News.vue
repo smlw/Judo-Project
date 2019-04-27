69f2131340c721b58e572576022ea70232441635
smlw <smlwmy@gmail.com>
2019-04-27 11:48:15 +0500

    create upcoming event component
    Signed-off-by: smlw <smlwmy@gmail.com>

--- src/components/pages/News.vue
+++ src/components/pages/News.vue
@@ -26,52 +26,18 @@
               h2 Грядущие события
               .swiper-slider
                 swiper(:options='swiperEvents')
-                  swiper-slide 
-                    a(href="/")
-                      .card_upcoming
-                        img(src="../../assets/images/events_1.png")
-                        .card_upcoming_description
-                          .card_upcoming_description_gradient
-                            .card_upcoming_description_date
-                              h3 "Зажги синим!" 2 апреля
-                            .card_upcoming_description_text
-                              p Масштабная городская акция «Зажги синим», приуроченная ко Всемирному дню распространения информации о проблеме аутизма.
-                  swiper-slide 
-                    a(href="/")
-                      .card_upcoming
-                        img(src="../../assets/images/events_1.png")
-                        .card_upcoming_description
-                          .card_upcoming_description_gradient
-                            .card_upcoming_description_date
-                              h3 ГОиЧС: безопасность весной
-                            .card_upcoming_description_text
-                              p С наступлением весны лед на реках становится рыхлым, «съедается» сверху солнцем, талой водой, а снизу подтачивается течением
-                  swiper-slide 
-                    a(href="/")
-                      .card_upcoming
-                        img(src="../../assets/images/events_1.png")
-                        .card_upcoming_description
-                          .card_upcoming_description_gradient
-                            .card_upcoming_description_date
-                              h3 Летняя оздоровительная кампания: 22 загородных и 132 городских лагеря
-                            .card_upcoming_description_text
-                              p Летняя оздоровительная кампания начнется 1 апреля...
-                  swiper-slide 
-                    a(href="/")
-                      .card_upcoming
-                        img(src="../../assets/images/events_1.png")
-                        .card_upcoming_description
-                          .card_upcoming_description_gradient
-                            .card_upcoming_description_date
-                              h3 Первенство Свердловской области 21.02.2019 город Екатеринбург.
-                            .card_upcoming_description_text
-                              p Уже сегодня пройдёт Первенство Свердловской области по дзюдо среди юношей и девушек до 13 лет...
+                  swiper-slide(v-for="(event, index) in 5" :key="index")
+                    router-link(to="/")
+                      j-upcoming-card
+                        img(slot="image" src="../../assets/images/events_1.png")
+                        h3(slot="title") {{index}} ГОиЧС: безопасность весной
+                        p(slot="description") С наступлением весны лед на реках становится рыхлым, «съедается» сверху солнцем, талой водой, а снизу подтачивается течением
                 .swiper-button-prev(slot='button-prev')
                 .swiper-button-next(slot='button-next')  
             .news_album
               h2 Фотогалерея
               .album_content
-                .album_content_item(v-for="(photoCard, index) in 4" :key="index")
+                .album_content_item(v-for="(photoCard, index) in 3" :key="index")
                   router-link(to="/test")
                     j-photo-preview(:settings="photoCardSettings")
                       img(slot="image" src="../../assets/images/events_1.png")
@@ -160,45 +126,6 @@ h2
   flex-direction row
   justify-content center
   margin 25px 0 0 0
-.card_upcoming
-  width 268px
-  height 293px
-  img 
-    height 100%
-    object-fit cover
-  &_description
-    display flex
-    flex-direction column
-    flex-wrap wrap
-    justify-content flex-end
-    align-content flex-start
-    align-items flex-start
-    line-height normal
-    font-family 'Noto Sans', sans-serif
-    color #fff
-    margin -293px 0 0 0
-    height 293px
-    &_date
-      font-family 'Noto Sans', sans-serif
-      font-style normal
-      font-weight bold
-      font-size 20px
-      line-height 27px
-      margin 0 0 9px 0
-    &_text
-      font-family 'Noto Sans', sans-serif
-      font-style italic
-      font-weight normal
-      font-size 18px
-      line-height 29px
-    &_gradient
-      height 100%
-      width 100%
-      overflow hidden
-      background rgba(0, 0, 0, 0.4)
-      backdrop-filter blur(4px)
-      box-sizing border-box
-      padding 10px 14px
 
 .news_video_album
   &_content
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -4,6 +4,7 @@ import NewCardPreview from '../components/elements/NewCardPreview.vue'
 import MainLayout from '../components/layouts/MainLayout.vue'
 import BreadCrumbs from '../components/elements/UI/BreadCrumbs.vue'
 import HumanCard from '../components/elements/HumanCard.vue'
+import UpcomingEventCard from '../components/elements/UpcomingEventCard.vue'
 
 export default {
   install (Vue) {
@@ -12,7 +13,8 @@ export default {
     Vue.component('j-photo-preview', PhotoPreview),
     Vue.component('j-new-preview', NewCardPreview),
     Vue.component('j-breadcrumbs', BreadCrumbs),
-    Vue.component('j-human-card', HumanCard)
+    Vue.component('j-human-card', HumanCard),
+    Vue.component('j-upcoming-card', UpcomingEventCard)
   }
 }
 
